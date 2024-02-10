5d33185fb18c2d696819769b6f27792a172c00db
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-10 16:53:47 -0700

    new installer

--- install.bash
+++ install.bash
@@ -4,18 +4,9 @@ echo "Flavor: Normal"
 echo "Github: https://github.com/HttpAnimation/E6hub"
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 sleep 2
-
-## Body
-# Download
-git clone -b stable https://github.com/HttpAnimation/E6hub.git
+git clone https://github.com/HttpAnimation/E6hub.git
 cd E6hub
 mv '[PUBLIC] clientconfig.json' clientconfig.json
-## Remove file
-rm .gitignore
 rm README.md
-## End
-cd ../
-
-# Foot
-echo "Done installing"
-rm install.bash
+rm UserFavDownloads/GITHUBDONTREMOVEME.md
+rm UserFavDownloads/jsonData/GITHUBDONTREMOVEME.md
