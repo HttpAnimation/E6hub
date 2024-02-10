c2d311fddb7636d3105b04bf2b73cfcc598c92d1
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-10 16:47:44 -0700

    adds install code

--- MakeCD.bash
+++ MakeCD.bash
@@ -1,3 +1,5 @@
 echo "This is only made for the dev system if you aren't well me don't run you will get errors"
 cd /home/httpanimations/Desktop
-wget 
+wget https://raw.githubusercontent.com/HttpAnimation/E6hub/stable/installNonRM.bash
+mv installNonRM.bash install.bash
+bash install.bash
