3542b96faff9b410c511bb2dd4ac3652a6945905
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-10 16:54:01 -0700

    removes the head

--- install.bash
+++ install.bash
@@ -1,9 +1,9 @@
-# Head
 echo "E6hub downloader"
 echo "Flavor: Normal"
 echo "Github: https://github.com/HttpAnimation/E6hub"
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 sleep 2
+
 git clone https://github.com/HttpAnimation/E6hub.git
 cd E6hub
 mv '[PUBLIC] clientconfig.json' clientconfig.json
