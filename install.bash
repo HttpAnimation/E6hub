326c7b7c19a35044646020254a1f2d492c6fa976
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-11 23:28:14 -0700

    adds a note for a rewrite

--- install.bash
+++ install.bash
@@ -1,12 +1,7 @@
 echo "E6hub downloader"
 echo "Flavor: Normal"
 echo "Github: https://github.com/HttpAnimation/E6hub"
+echo "Note: This script it going under a rewrite"
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 sleep 2
 
-git clone https://github.com/HttpAnimation/E6hub.git
-cd E6hub
-mv '[PUBLIC] clientconfig.json' clientconfig.json
-rm README.md
-rm UserFavDownloads/GITHUBDONTREMOVEME.md
-rm UserFavDownloads/jsonData/GITHUBDONTREMOVEME.md
