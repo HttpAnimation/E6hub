1f97a05e1d2d2a84a004ee5d1ce29a61e0bc6401
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-11 23:29:27 -0700

    adds a .

--- install.bash
+++ install.bash
@@ -1,7 +1,7 @@
 echo "E6hub downloader"
 echo "Flavor: Normal"
 echo "Github: https://github.com/HttpAnimation/E6hub"
-echo "Note: This script it going under a rewrite"
+echo "Note: This script it going under a rewrite."
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 sleep 2
 
